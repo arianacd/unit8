315ecd9f91337a2d3fe4a500aa26281ef5d08ede
ariana <ariana.daney@ssfs.org>
2019-11-18 11:19:27 -0500

    worked on daily work

--- daily_exercises.py
+++ daily_exercises.py
@@ -11,8 +11,21 @@ root = Tk()
 # hello_world.grid(row=1, column=1)
 
 # ex. 2
-user_name = StringVar
-greeting = Entry(root)
-greeting.grid(row=1, column=1)
-hello_greeting = Label(root, text="Hello," + user_name)
+
+
+def say_hi():
+    if user_name == "":
+        print("you forgot to enter to your name")
+    else:
+        print("hello", user_name)
+
+
+user_name = StringVar()
+entry_name = Entry(root, textvariable=user_name)
+entry_name.grid(row=1, column=1)
+hello = Label(root)
+hello.grid(row=2, column=1)
+say_hello = Button(root, text="Say Hello")
+say_hello.grid(row=3, column=1)
+
 root.mainloop()
